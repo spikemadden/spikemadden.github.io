26c2d772e52a22e0b3676045f37d1f26b5b857ed
Spike Madden <madden.spike@gmail.com>
2019-08-30 17:06:43 -0700

    new website :running:

--- emoji-typer/js/scripts.js
+++
@@ -1,7 +0,0 @@
-$('.original').on('keyup', function() {
-  let input = $(this).val();
-  let emoji = $('#dropdown').val();
-  console.log(emoji);
-  let result = input.split(' ').join(emoji);
-  $('.mirror').val(result);
-});
